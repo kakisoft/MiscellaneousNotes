6040748c8c14501ad05dddd8772baae8ccc33aa1
kakisoft <kakisoft@gmail.com>
2017-11-15 21:20:03 +0900

    Update vb_snippet.vb

--- vb_snippet.vb
+++ vb_snippet.vb
@@ -1,7 +1,36 @@
-## ActiveReport6
-コピー操作禁止
-```vb
+'---------------------
+'   ActiveReport6
+'---------------------
+'PDFコピー禁止
 Dim ReportFile As Export.Pdf.PdfExport = New Export.Pdf.PdfExport
 ReportFile.Security.Permissions = Export.Pdf.PdfPermissions.None
-```
 
+'---------------------
+'   Noghing check
+'---------------------
+If IsNothing(anInstance) Then
+    //do
+End If
+
+If Not IsNothing(anInstance) Then
+    //do
+End If
+
+If anInstance Is Nothing Then
+    //do
+End If
+
+If anInstance IsNot Nothing Then
+    //do
+End If
+
+'---------------------
+'     Dictionary
+'---------------------
+Dim names As New Dictionary(Of String, String)
+names.Add("江戸幕府", "徳川家康")
+names.Add("室町幕府", "足利尊氏")
+names.Add("鎌倉幕府", "源頼朝")
+
+Dim edoStarter As String
+edoStarter = names("江戸幕府")
