d63f60cfa3a097745a4dab7aa873b82476b239a4
kakisoft <kakisoft@gmail.com>
2018-03-16 10:37:14 +0900

    Create 02_2_grant_authority2.sql

---
+++ Oracle/12/02_2_grant_authority2.sql
@@ -0,0 +1,10 @@
+grant execute on SYS.UTL_ENCODE to PUBLIC;
+grant execute on SYS.UTL_RAW to PUBLIC;
+grant execute on SYS.UTL_I18N to PUBLIC
+grant execute on SYS.UTL_SMTP to PUBLIC;
+grant execute on SYS.DBMS_LOB to PUBLIC;
+grant execute on SYS.DBMS_RANDOM to PUBLIC;
+grant execute on SYS.DBMS_DEBUG_JDWP to PUBLIC;
+
+grant debug connect session to PUBLIC;
+grant debug any procedure to PUBLIC;
