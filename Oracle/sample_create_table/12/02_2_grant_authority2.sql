29ac37e68d20c9e3590a3cb8b112a8e8d9796067
kakisoft <kakisoft@gmail.com>
2018-03-19 10:15:01 +0900

    Update 02_2_grant_authority2.sql

--- Oracle/sample_create_table/12/02_2_grant_authority2.sql
+++ Oracle/sample_create_table/12/02_2_grant_authority2.sql
@@ -5,6 +5,7 @@ grant execute on SYS.UTL_SMTP to PUBLIC;
 grant execute on SYS.DBMS_LOB to PUBLIC;
 grant execute on SYS.DBMS_RANDOM to PUBLIC;
 grant execute on SYS.DBMS_DEBUG_JDWP to PUBLIC;
+grant execute on DBMS_DEBUG_JDWP to PUBLIC;
 
 grant execute on SYS.UTL_HTTP to PUBLIC;
 grant execute on SYS.UTL_TCP to PUBLIC;
