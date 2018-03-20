8b2c42bd33ef7fa7d12289d521783db48534328c
kakisoft <kakisoft@gmail.com>
2018-03-20 13:42:27 +0900

    Update BODY_MY_PACKAGE_01.sql

--- Oracle/sample_package/BODY_MY_PACKAGE_01.sql
+++ Oracle/sample_package/BODY_MY_PACKAGE_01.sql
@@ -1,7 +1,9 @@
 create or replace PACKAGE BODY MY_PACKAGE_01 IS
 
   --===================================
+  -- 
   --          MY_PROCEDURE_01
+  -- 
   --===================================
   PROCEDURE MY_PROCEDURE_01 IS
     privateNumber number := 100;
@@ -115,7 +117,9 @@ create or replace PACKAGE BODY MY_PACKAGE_01 IS
   END MY_PROCEDURE_01;
 
   --===================================
+  -- 
   --          MY_PROCEDURE_02
+  -- 
   --===================================
   PROCEDURE MY_PROCEDURE_02 (
                                IN_PARAM1 IN NUMBER
@@ -186,7 +190,9 @@ create or replace PACKAGE BODY MY_PACKAGE_01 IS
   END MY_PROCEDURE_02;
 
   --===================================
+  --   
   --          MY_PROCEDURE_03
+  -- 
   --===================================
   PROCEDURE MY_PROCEDURE_03 (
                                IN_PARAM1 IN NUMBER
